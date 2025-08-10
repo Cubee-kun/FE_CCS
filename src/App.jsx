df55954885595a30f7844d255a7883dffad393c7
Cubee-kun <ibraveaslam73@gmail.com>
2025-08-11 04:10:52 +0700

    main and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import AppRoutes from './routes/AppRoutes';
-import './index.css'
+// src/App.jsx
+import { useLocation } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./components/Navbar";
+import "./index.css";
 
 function App() {
+  const location = useLocation();
+
+  // Daftar path dimana Navbar tidak ditampilkan
+  const noNavbarRoutes = ["/login", "/register"];
+
+  // Cek apakah current path ada di daftar noNavbarRoutes (mencakup kemungkinan query params)
+  const hideNavbar = noNavbarRoutes.some((path) => location.pathname.startsWith(path));
+
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      {!hideNavbar && <Navbar />}
+      <AppRoutes />
     </AuthProvider>
   );
 }
---
+++ src/components/AppLayout.jsx
@@ -0,0 +1,14 @@
+// components/AppLayout.jsx
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+
+export default function AppLayout() {
+  return (
+    <div className="flex bg-gray-50 min-h-screen">
+      <Sidebar />
+      <main className="flex-1 ml-64 p-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
